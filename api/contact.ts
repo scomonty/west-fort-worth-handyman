5675e06c22725fa0125c2a11578b1e3d6fb7475d
Scott Montgomery <scottmontgomery@Scotts-MacBook-Pro.local>
2026-03-10 12:56:33 -0500

    update send.

--- api/contact.ts
+++ api/contact.ts
@@ -58,7 +58,7 @@ export default async function handler(req, res) {
     // Send confirmation email to customer
 if (email) {
   await resend.emails.send({
-    from: "West Fort Worth Handyman <hello@send.fortworthhandyman.com>",
+    from: "West Fort Worth Handyman <estimates@westfortworthhandyman.com>",
     to: email,
     subject: "We received your request",
     html: `
